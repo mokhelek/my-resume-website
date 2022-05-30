311bd6d45c67bd9098309dbc7f3b5254c800f073
mokhelek <mkatleho191@gmail.com>
2022-05-30 23:39:16 +0200

    removed database

--- dennisivy/settings.py
+++ dennisivy/settings.py
@@ -99,12 +99,13 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+""" 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
